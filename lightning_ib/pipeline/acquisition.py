b35f7e1a461e6b5cc5c94dec5a20f21f43bf0f59
Justin Goheen <26209687+JustinGoheen@users.noreply.github.com>
2022-12-02 12:07:37 -0500

    add data pipeline scripts

--- lightning_ib/pipeline/acquisition.py
+++ lightning_ib/pipeline/acquisition.py
@@ -7,8 +7,9 @@ from rich import print as rprint
 
 filepath = Path(__file__)
 projectpath = filepath.parents[2]
-datapath = os.path.join(projectpath, "data", "markets")
-marketsblobpath = os.path.join(datapath, "markets.json")
+marketspath = os.path.join(projectpath, "data", "markets")
+rawdatapath = os.path.join(marketspath, "raw")
+marketsblobpath = os.path.join(marketspath, "markets.json")
 
 with open(marketsblobpath) as f:
     markets = json.load(f)
@@ -19,8 +20,8 @@ for key in markets.keys():
 
     rprint(f"[bold white]{key.upper()}[/bold white]")
 
-    if not os.path.isdir(os.path.join(datapath, key)):
-        os.mkdir(os.path.join(datapath, key))
+    if not os.path.isdir(os.path.join(rawdatapath, key)):
+        os.mkdir(os.path.join(rawdatapath, key))
 
     for ticker in markets[key]:
         if ticker == "VIX":
@@ -33,7 +34,7 @@ for key in markets.keys():
 
         rprint(f"[bold green]{ticker}[/bold green]: start {tickerdata.index[0]} end {tickerdata.index[-1]}")
 
-        tickerdatapath = os.path.join(datapath, key, f"{ticker}.pq")
+        tickerdatapath = os.path.join(rawdatapath, key, f"{ticker}.pq")
         tickerdata.to_parquet(tickerdatapath)
 
 rprint("\n" + f"[bold cyan]DATA FETCHED[/bold cyan]" + "\n")
--- lightning_ib/pipeline/preprocess.py
+++ lightning_ib/pipeline/preprocess.py
@@ -13,3 +13,53 @@
 # limitations under the License.
 
 # use this to create a pre-processing script if needed
+
+import os
+import json
+import yfinance as yf
+
+from pathlib import Path
+from rich import print as rprint
+from pyarrow import parquet as pq
+
+from lightning_ib.metrics import factors
+
+
+FILEPATH = Path(__file__)
+PROJECTPATH = FILEPATH.parents[2]
+MARKETSPATH = os.path.join(PROJECTPATH, "data", "markets")
+RAWDATAPATH = os.path.join(MARKETSPATH, "raw")
+PROCESSEDDATAPATH = os.path.join(MARKETSPATH, "processed")
+MARKETSBLOBPATH = os.path.join(MARKETSPATH, "markets.json")
+
+with open(MARKETSBLOBPATH) as f:
+    markets = json.load(f)
+
+rprint("\n" + f"[bold cyan]PRE PROCESSING[/bold cyan]" + "\n")
+
+for key in markets.keys():
+
+    if not os.path.isdir(os.path.join(PROCESSEDDATAPATH, key)):
+        os.mkdir(os.path.join(PROCESSEDDATAPATH, key))
+
+    for ticker in markets[key]:
+
+        rawtickerdatapath = os.path.join(RAWDATAPATH, key, f"{ticker}.pq")
+        processedtickerdatapath = os.path.join(PROCESSEDDATAPATH, key, f"{ticker}.pq")
+
+        data = pq.read_table(rawtickerdatapath, columns=["Date", "Open", "High", "Low", "Close"]).to_pandas()
+
+        if ticker == "VIX":
+            data["RANK"] = factors.expanding_rank(data["Close"])
+        else:
+            data["NATR_RANK"] = factors.expanding_rank(factors.normalized_average_true_range(data, period=20))
+            data["AROON_RANK"] = factors.expanding_rank(factors.aroon_oscillator(data, period=20))
+            data["RSI_RANK"] = factors.expanding_rank(factors.relative_strength_index(data["Close"], period=20))
+            data["ROC_RANK"] = factors.expanding_rank(factors.rate_of_change(data["Close"], period=20))
+            data["RTNS_RANK"] = factors.expanding_rank(factors.log_returns(data["Close"]))
+
+        data.drop(["Open", "High", "Low", "Close"], axis=1, inplace=True)
+
+        data.to_parquet(processedtickerdatapath)
+
+rprint("\n" + f"[bold cyan]DATA PROCESSED[/bold cyan]" + "\n")
