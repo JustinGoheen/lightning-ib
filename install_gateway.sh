2a07b004a2a0770db5a096eb9fe83eddf68dd0e4
Justin Goheen <26209687+JustinGoheen@users.noreply.github.com>
2022-12-21 08:08:11 -0500

    add cloud install scripts

---
+++ install_gateway.sh
@@ -0,0 +1,4 @@
+echo "Installing Gateway"
+wget https://download2.interactivebrokers.com/installers/ibgateway/stable-standalone/ibgateway-stable-standalone-linux-x64.sh
+chmod u+x ibgateway-stable-standalone-linux-x64.sh
+./ibgateway-stable-standalone-linux-x64.sh
